86d8a1065c347095069f00196af85c7e94439be7
Atikuzzaman Ratan <azamanru@gmail.com>
2026-04-05 15:31:10 +0600

    Institute and municipal list API created

---
+++ AppsAPI/UserAssignedInstituteList.php
@@ -0,0 +1,38 @@
+<?php
+require '../vendor/autoload.php';
+
+use Solvers\Dsql\Application;
+
+$app = new Application();
+
+include '../Config/config.php';
+
+//https://scempbbs.com/AppsAPI/UserAssignedInstituteList.php?userid=69
+
+$UserId = $_REQUEST['userid'];
+$formId = $formIdSamplingData;
+
+$query = "
+IF object_id('tempdb..##TempSMData') is not null
+	BEGIN
+		drop table ##TempInstData;
+	END
+CREATE TABLE ##TempInstData(ID INT, Bsic_Code VARCHAR(50), MobileNo VARCHAR(50), InstName NVARCHAR(MAX));
+
+INSERT ##TempInstData (ID, Bsic_Code, MobileNo, InstName)
+SELECT id, BSIC_CODE, MOBILE_NO, RTRIM(LTRIM(Q4A)) InstName FROM InstituteInfo WHERE UserID = '$UserId' AND Type = 'Establishment'
+AND id NOT IN (SELECT SampleHHNo FROM xformrecord WHERE FormId='$formId' AND UserID = '$UserId') ORDER BY id ASC;";
+$app->getDBConnection()->Query($query);
+
+$query2 = "Declare @InstList AS nvarchar(max);
+SELECT @InstList = COALESCE(@InstList + '~~~', '') + CONCAT('ID_', ID, '_BSICCODE_', Bsic_Code, '_MOBILE_', MobileNo, '_NAME_', InstName) 
+FROM ##TempInstData ORDER BY ID ASC; 
+select @InstList as InstNumbers";
+
+$InstList = $app->getDBConnection()->query($query2);
+
+foreach ($InstList as $row) {
+    $UserAssignedInst = $row->InstNumbers;
+    echo $UserAssignedInst;
+}
+
---
+++ AppsAPI/UserAssignedMunicipalList.php
@@ -0,0 +1,37 @@
+<?php
+require '../vendor/autoload.php';
+
+use Solvers\Dsql\Application;
+
+$app = new Application();
+
+include '../Config/config.php';
+
+//https://scempbbs.com/AppsAPI/UserAssignedMunicipalList.php?userid=69
+
+$UserId = $_REQUEST['userid'];
+$formId = $formIdMainData;
+
+$query = "
+IF object_id('tempdb..##TempMunData') is not null
+	BEGIN
+		drop table ##TempMunData;
+	END
+CREATE TABLE ##TempMunData(ID INT, Division VARCHAR(50), District VARCHAR(50), InstName NVARCHAR(MAX));
+
+INSERT ##TempMunData (ID, Division, District, InstName)
+SELECT id, DIVISION_NAME, DISTRICT_NAME, RTRIM(LTRIM(Q4A)) InstName FROM InstituteInfo WHERE UserID = '$UserId' AND Type = 'Municipal'
+AND id NOT IN (SELECT SampleHHNo FROM xformrecord WHERE FormId='$formId' AND UserID = '$UserId') ORDER BY id ASC;";
+$app->getDBConnection()->Query($query);
+
+$query2 = "Declare @MunList AS nvarchar(max);
+SELECT @MunList = COALESCE(@MunList + '~~~', '') + CONCAT('ID_', ID, '_DIV_', Division, '_DIST_', District, '_NAME_', InstName) 
+FROM ##TempMunData ORDER BY ID ASC; 
+select @MunList as MunNumbers";
+$MunList = $app->getDBConnection()->query($query2);
+
+foreach ($MunList as $row) {
+    $UserAssignedMun = $row->MunNumbers;
+    echo $UserAssignedMun;
+}
+
