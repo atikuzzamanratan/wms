4f52e5078a662e886526d5b66cb10f7da0fb8aed
Atikuzzaman Ratan <azamanru@gmail.com>
2026-05-12 11:34:37 +0600

    Document and help-support updated

--- AppsAPI/AppsDocuments.php
+++ AppsAPI/AppsDocuments.php
@@ -17,22 +17,67 @@ if ($AuthToken != $AuthTokenValue) {
 include_once '../Components/header-includes.php';
 ?>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Icons CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+
     <section class="card">
         <header class="card-header">
             <div class="card-title"><h4>Useful Documents</h4></div>
         </header>
         <div class="card-body">
             <table class="table table-bordered table-striped">
+                <tr>
+                    <th style="color: rebeccapurple">ম্যানুয়াল</th>
+                </tr>
                 <tbody>
                 <tr>
-                    <td><a href="../Documents/WMS-Mobile-Application-Manual-v3"><i class="bi bi-phone"></i>মোবাইল এপ্লিকেশন ম্যানুয়াল</a></td>
-                </tr>
-                <tr>
-                    <td><a href="../Documents/EPER-WM-Survey-2026_Questionnaire.pdf"><i class="bi bi-phone"></i>প্রশ্নপত্র: পরিবেশ সংরক্ষণ ব্যয়, সম্পদ এবং বর্জ্য ব্যবস্থাপনা (প্রতিষ্ঠান) জরিপ ২০২৬ (EPER & WM Survey)</a>
+                    <td>
+                        <a href="../Documents/WMS-Mobile-Application-Manual-v3">
+                            <i class="bi bi-book"></i> মোবাইল এপ্লিকেশন ম্যানুয়াল
+                        </a>
                     </td>
                 </tr>
                 <tr>
-                    <td><a href="../Documents/MWM-Survey-2026-Questionnarie.pdf"><i class="bi bi-phone"></i>প্রশ্নপত্র: বর্জ্য ব্যবস্থাপনা জরিপ (সিটি কর্পোরেশন ও পৌরসভা) ২০২৬ (MWM Survey)</a>
+                    <td>
+                        <a href="../Documents/Training-Manual-EPER-WM-Survey-2026.pdf">
+                            <i class="bi bi-book"></i> প্রশিক্ষণ ম্যানুয়াল - পরিবেশ সংরক্ষণ ব্যয়, সম্পদ এবং বর্জ্য
+                            ব্যবস্থাপনা (প্রতিষ্ঠান)
+                            জরিপ ২০২৬
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="../Documents/Training-Manual-MWM-Survey-2026.pdf">
+                            <i class="bi bi-book"></i> প্রশিক্ষণ ম্যানুয়াল - বর্জ্য ব্যবস্থাপনা জরিপ (সিটি কর্পোরেশন ও
+                            পৌরসভা) ২০২৬
+                        </a>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+
+            <table class="table table-bordered table-striped">
+                <tr>
+                    <th style="color: rebeccapurple">প্রশ্নপত্র</th>
+                </tr>
+                <tbody>
+                <tr>
+                    <td>
+                        <a href="../Documents/EPER-WM-Survey-2026_Questionnaire.pdf">
+                            <i class="bi bi-book"></i> প্রশ্নপত্র: পরিবেশ সংরক্ষণ ব্যয়, সম্পদ এবং বর্জ্য ব্যবস্থাপনা
+                            (প্রতিষ্ঠান) জরিপ ২০২৬ (EPER & WM Survey)
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="../Documents/MWM-Survey-2026-Questionnarie.pdf">
+                            <i class="bi bi-book"></i> প্রশ্নপত্র: বর্জ্য ব্যবস্থাপনা জরিপ (সিটি কর্পোরেশন ও পৌরসভা)
+                            ২০২৬ (MWM Survey)
+                        </a>
                     </td>
                 </tr>
 
--- AppsAPI/HelpNSupport.php
+++ AppsAPI/HelpNSupport.php
@@ -16,6 +16,9 @@ if ($AuthToken != $AuthTokenValue) {
 
 include_once '../Components/header-includes.php';
 ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap Icons CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 
     <section class="card">
         <header class="card-header">
@@ -24,12 +27,15 @@ include_once '../Components/header-includes.php';
         <div class="card-body">
             <table class="table table-striped table-bordered">
                 <tr>
-                    <th>ROBI Help Line</th>
+                    <th style="color: firebrick"><b>ROBI Help Line</b></th>
                 </tr>
                 <tr>
                     <td>
-						<a href="tel:+8801880004420"><i class="bi bi-phone"></i>ROBI Govt. Project Hotline Number<br>+8801880004420</a> 
-						<br/>[যখন ফোন করবেন তখন বলবেন `আপনি বি বি এস থেকে ফোন করছেন` বলে সমস্যার কথা জানাবেন]
+                        <b>ROBI Govt. Project Hotline Number:</b><br>
+                        <a href="tel:+8801880004420">
+                            <i class="bi bi-phone"></i> <b> +8801880004420</b>
+                        </a>
+                        <br/>[যখন ফোন করবেন তখন বলবেন `আপনি বি বি এস থেকে ফোন করছেন` বলে সমস্যার কথা জানাবেন]
                     </td>
                 </tr>
                 <tr>
@@ -38,57 +44,76 @@ include_once '../Components/header-includes.php';
                 <tr>
                     <td>ব্যলান্স চেক/বকেয়া বিল *1#</td>
                 </tr>
+            </table>
+            <table class="table table-striped table-bordered">
                 <tr>
-                    <th>Project Head quarter</th>
+                    <th style="color: blue"><b>Project Head quarter</b></th>
                 </tr>
                 <tr>
-                    <td><a href="tel:+8801712141750"><i class="bi bi-phone"></i>
-                            Md. Rafiqul Islam<br>
-                            Director (In Charge)<br>
-                            Director General Office,<br>
-                            Bangladesh Bureau of Statistics<br>
-                            (Additional Charge:SCEMP Project, BBS)<br>
-                            Mobile: +8801712141750</a>
+                    <td>
+                        <b>Md. Rafiqul Islam</b><br>
+                        Director (In Charge)<br>
+                        Director General Office,<br>
+                        Bangladesh Bureau of Statistics<br>
+                        (Additional Charge: SCEMP Project, BBS)<br>
+                        <a href="tel:+8801712141750">
+                            <i class="bi bi-phone"></i> <b> Mobile: +8801712141750</b>
+                        </a>
                     </td>
                 </tr>
                 <tr>
-                    <td><a href="tel:+8801727301205"><i class="bi bi-phone"></i>
-                            Surangit Kumar Ghosh<br>
-                            Deputy Director<br>
-                            National Accounting Wing<br>
-                            Bangladesh Bureau of Statistics<br>
-                            Mobile: +8801727301205</a>
+                    <td>
+                        <b>Surangit Kumar Ghosh</b><br>
+                        Deputy Director<br>
+                        National Accounting Wing<br>
+                        Bangladesh Bureau of Statistics<br>
+                        <a href="tel:+8801727301205">
+                            <i class="bi bi-phone"></i> <b> Mobile: +8801727301205</b>
+                        </a>
                     </td>
                 </tr>
                 <tr>
-                    <td><a href="tel:+8801310724965"><i class="bi bi-phone"></i>
-                            Ahshan Habib<br>
-                            Statistical Officer, FA & MIS<br>
-                            Bangladesh Bureau of Statistics<br>
-                            Mobile: +8801310724965</a>
+                    <td>
+                        <b>Ahshan Habib</b><br>
+                        Statistical Officer, FA & MIS<br>
+                        Bangladesh Bureau of Statistics<br>
+                        <a href="tel:+8801310724965">
+                            <i class="bi bi-phone"></i><b> Mobile: +8801310724965</b>
+                        </a>
+                    </td>
+                </tr>
+            </table>
+            <table class="table table-striped table-bordered">
+                <tr>
+                    <th style="color: rebeccapurple">Solvers Support (9:15 A.M to 5:00 P.M)</th>
+                </tr>
+                <tr>
+                    <td>
+                        1st Line Support:
+                        <a href="tel:+8801720540859">
+                            <i class="bi bi-phone"></i> +8801720540859
+                        </a>
                     </td>
                 </tr>
                 <tr>
-                    <th>Solvers Support (9:15 A.M to 5:00 P.M)</th>
-                </tr>
-                <tr>
-                    <td><a href="tel:+8801720540859"><i class="bi bi-phone"></i>1st Line Support
-                            +8801720540859</a>
+                    <td>2nd Line Support:
+                        <a href="tel:+8801810096285">
+                            <i class="bi bi-phone"></i> +8801810096285
+                        </a>
                     </td>
                 </tr>
                 <tr>
-                    <td><a href="tel:+8801810096285"><i class="bi bi-phone"></i>2nd Line Support
-                            +8801810096285</a>
+                    <td>3rd Line Support:
+                        <a href="tel:+8801329684382">
+                            <i class="bi bi-phone"></i> +8801329684382
+                        </a>
                     </td>
                 </tr>
-				<tr>
-                    <td><a href="tel:+8801329684382"><i class="bi bi-phone"></i>3rd Line Support
-                            +8801329684382</a>
-                    </td>
-                </tr>
-				<tr>
-                    <td><a href="tel:+8801841190330"><i class="bi bi-phone"></i>4th Line Support
-                            +8801841190330</a>
+                <tr>
+                    <td>4th Line Support:
+                        <a href="tel:+8801841190330">
+                            <i class="bi bi-phone"></i> +8801841190330
+                        </a>
                     </td>
                 </tr>
             </table>
